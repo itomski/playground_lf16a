60fc754009173cb257ccca0fa5323e2ae28b1638
tlubowiecki <tomasz@lubowiecki.de>
2025-12-17 09:10:49 +0100

    Coins

---
+++ src/main/java/de/gfn/playground/datanbanken/Abfrage.java
@@ -0,0 +1,35 @@
+package de.gfn.playground.datanbanken;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Abfrage {
+
+    private Map<Integer, Integer> coins = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        new Abfrage().fillCoins();
+
+    }
+
+    private void fillCoins() {
+        try(Connection dbh = null; Statement stmt = dbh.createStatement()) {
+
+            final String SQL = "SELECT * FROM Münzbestand";
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            while (rs.next()) {
+                coins.put(rs.getInt("Münztyp"), rs.getInt("Anzahl"));
+            }
+
+            //... coins ist befüllt
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
